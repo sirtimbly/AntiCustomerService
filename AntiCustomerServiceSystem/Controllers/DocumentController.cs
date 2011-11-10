b14d5bbd2eb051ce268f86c4c618a5a7c80a4f38
Tim Bendt <sirtimbly@gmail.com>
2011-11-09 17:29:32 -0700

    added document upload to issues

--- AntiCustomerServiceSystem/Controllers/DocumentController.cs
+++ AntiCustomerServiceSystem/Controllers/DocumentController.cs
@@ -6,103 +6,138 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using AntiCustomerServiceSystem.Models;
+using System.Web.Helpers;
+using System.IO;
 
 namespace AntiCustomerServiceSystem.Controllers
 { 
-    public class DocumentController : Controller
-    {
-        private ServiceSystemContainer db = new ServiceSystemContainer();
+	public class DocumentController : Controller
+	{
+		private ServiceSystemContainer db = new ServiceSystemContainer();
 
-        //
-        // GET: /Document/
+		//
+		// GET: /Document/
 
-        public ViewResult Index()
-        {
-            return View(db.Documents.ToList());
-        }
+		public ViewResult Index()
+		{
+			return View(db.Documents.ToList());
+		}
 
-        //
-        // GET: /Document/Details/5
+		//
+		// GET: /Document/Details/5
 
-        public ViewResult Details(int id)
-        {
-            Document document = db.Documents.Find(id);
-            return View(document);
-        }
+		public ViewResult Details(int id)
+		{
+			Document document = db.Documents.Find(id);
+			return View(document);
+		}
 
-        //
-        // GET: /Document/Create
+		//
+		// GET: /Document/Create
 
-        public ActionResult Create()
-        {
-            return View();
-        } 
+		public ActionResult Create(int? issueId = null, int? companyId = null)
+		{
+			ViewBag.IssueId = issueId;
+			ViewBag.CompanyId = companyId;
+			return View();
+		}
 
-        //
-        // POST: /Document/Create
+		public ActionResult Upload(FormCollection form)
+		{
+			string fileSavePath = "";
+			string uploadDir = "/Documents/";
+			var uploadedFile = Request.Files[0];
+			string fileName = Path.GetFileName(uploadedFile.FileName);
+			fileSavePath = Server.MapPath(uploadDir + fileName);
+			uploadedFile.SaveAs(fileSavePath);
+			Document document = new Document();
+			document.Modified = document.Created = DateTime.Now;
+			document.URI = uploadDir + fileName;
+			if (!String.IsNullOrWhiteSpace(form["issueId"]))
+			{
+				Issue issue = db.Issues.Find(int.Parse(form["issueId"]));
+				document.Issue = issue;
+				db.Documents.Add(document);
+				db.SaveChanges();
+				return RedirectToAction("Details", "Issue", issue);
+			}
+			if (!String.IsNullOrWhiteSpace(form["companyId"]))
+			{
+				Company company = db.Companies.Find(int.Parse(form["companyId"]));
+				document.Company = company;
+				db.Documents.Add(document);
+				db.SaveChanges();
+				return RedirectToAction("Details", "Company", company);
+			}
 
-        [HttpPost]
-        public ActionResult Create(Document document)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Documents.Add(document);
-                db.SaveChanges();
-                return RedirectToAction("Index");  
-            }
+			return RedirectToAction("Index");
+		}
 
-            return View(document);
-        }
-        
-        //
-        // GET: /Document/Edit/5
+		//
+		// POST: /Document/Create
+
+		[HttpPost]
+		public ActionResult Create(Document document, FormCollection form)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Documents.Add(document);
+				db.SaveChanges();
+				return RedirectToAction("Index");  
+			}
+
+			return View(document);
+		}
+		
+		//
+		// GET: /Document/Edit/5
  
-        public ActionResult Edit(int id)
-        {
-            Document document = db.Documents.Find(id);
-            return View(document);
-        }
+		public ActionResult Edit(int id)
+		{
+			Document document = db.Documents.Find(id);
+			return View(document);
+		}
 
-        //
-        // POST: /Document/Edit/5
+		//
+		// POST: /Document/Edit/5
 
-        [HttpPost]
-        public ActionResult Edit(Document document)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(document).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(document);
-        }
+		[HttpPost]
+		public ActionResult Edit(Document document)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(document).State = EntityState.Modified;
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(document);
+		}
 
-        //
-        // GET: /Document/Delete/5
+		//
+		// GET: /Document/Delete/5
  
-        public ActionResult Delete(int id)
-        {
-            Document document = db.Documents.Find(id);
-            return View(document);
-        }
+		public ActionResult Delete(int id)
+		{
+			Document document = db.Documents.Find(id);
+			return View(document);
+		}
 
-        //
-        // POST: /Document/Delete/5
+		//
+		// POST: /Document/Delete/5
 
-        [HttpPost, ActionName("Delete")]
-        public ActionResult DeleteConfirmed(int id)
-        {            
-            Document document = db.Documents.Find(id);
-            db.Documents.Remove(document);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+		[HttpPost, ActionName("Delete")]
+		public ActionResult DeleteConfirmed(int id)
+		{            
+			Document document = db.Documents.Find(id);
+			db.Documents.Remove(document);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
 
-        protected override void Dispose(bool disposing)
-        {
-            db.Dispose();
-            base.Dispose(disposing);
-        }
-    }
+		protected override void Dispose(bool disposing)
+		{
+			db.Dispose();
+			base.Dispose(disposing);
+		}
+	}
 }
